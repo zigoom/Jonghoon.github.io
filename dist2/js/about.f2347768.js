9beb8584e7797d758e16d619f83fd9de5305dceb
김종훈 <rlawhdgns72@naver.com>
2023-06-25 17:07:42 +0900

    no message

---
+++ dist2/js/about.f2347768.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkblog"]=self["webpackChunkblog"]||[]).push([[443],{833:function(n,u,t){t.r(u),t.d(u,{default:function(){return b}});var a=t(252);const e={class:"about"},r=(0,a._)("h1",null,"This is an about page",-1),s=[r];function c(n,u){return(0,a.wg)(),(0,a.iD)("div",e,s)}var o=t(744);const i={},l=(0,o.Z)(i,[["render",c]]);var b=l}}]);
+//# sourceMappingURL=about.f2347768.js.map
