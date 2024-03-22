9143967907c20bbe5510ad5f686a5270b8701fb3
김종훈 <rlawhdgns72@naver.com>
2024-03-22 21:00:52 +0900

    [24.03.22] 스타일 부락부분 수정

--- src/components/Other.vue
+++ src/components/Other.vue
@@ -1,8 +1,8 @@
 <template>
     <p class="m-2"></p>
-    <hr />
+    <!-- <hr /> -->
     <!-- eslint-disable-next-line vue/no-multiple-template-root -->
-    <div class="row row-cols-1 row-cols-sm-3 g-2 m-0">
+    <div class="row row-cols-1 row-cols-sm-3 g-2 m-0" style="padding: 5px 80px 10px 80px;">
       <!-- row 안에 col로 Grid 형태를 만든다. row-cols-1 row-cols-sm-3 는 행에 몇게를 나오게 할지이고, g-2 는 셀간격,    -->
       <div class="col"  v-for="data in filteredProjectList" :key="data.id" >
         <list-card :data="data"  />
